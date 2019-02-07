380d654b032f79d969c473a8fcd765a29ab646fb
Chrissy Zwainz <zchrissychrissy@gmail.com>
2019-02-06 17:08:44 -0700

    :woman_shrugging:

---
+++ app.js
@@ -0,0 +1,39 @@
+let choices = {
+  rock: {
+    image: "https://images.unsplash.com/photo-1444312645910-ffa973656eba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80",
+    beats: 'scissors'
+  },
+  paper: {
+    image: "https://images.unsplash.com/photo-1507925921958-8a62f3d1a50d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1055&q=80",
+    beats: 'rock'
+  },
+  scissors: {
+    image: "https://images.unsplash.com/photo-1503792501406-2c40da09e1e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80",
+    beats: 'paper'
+  }
+}
+
+//dynamically render RPS
+function drawRPS() {
+  let template = ''
+  for (let key in choices) {
+    let item = choices[key]
+    template += `
+      <div class="col-4 justify-content-center">
+        <img src="${item.image}" class="rounded shadow mt-2" height="200px" width="200px">
+
+          <button onclick="play('${key}')" class="btn btn-info my-2">${key}</button>
+        </div>
+        `
+    document.getElementById('imageButtons').innerHTML = template
+  }
+}
+drawRPS()
+
+let computerChoice = Math.floor(Math.random() * 3)
+
+
+function play(playerChoice) {
+  let result = ''
+
+}
